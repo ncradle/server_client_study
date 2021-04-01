002a9eec160621cf6ec2f9df7f2836e1e043c286
ncradle <shinmy.mt555@gmail.com>
2021-04-02 00:53:39 +0900

    [add]server
    add socket communication server(tcp)

---
+++ server/include/server.h
@@ -0,0 +1,10 @@
+#ifndef SERVER_H_
+#define SERVER_H_
+
+#include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+#endif
---
+++ server/src/server.cc
@@ -0,0 +1,43 @@
+#include "server.h"
+
+int main()
+{
+  const uint16_t SERVER_PORT = 12345;
+  const int MAX_CLIENT = 5;
+
+  // 1:socket
+  // A socket is created with socket
+  // AF_INET     : ipv4
+  // SOCK_STREAM : tcp
+  int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+  // 2:bind
+  // The socket is bound to a local address using bind
+  // so that other sockets may be "connect"ed to it.
+  struct sockaddr_in server_addr;
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(SERVER_PORT);
+  server_addr.sin_addr.s_addr = INADDR_ANY;
+  bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr));
+
+  // 3:listen
+  // A willingness to accept incoming connections and
+  // a queue limit for incoming connections are specified with listen
+  listen(server_socket, MAX_CLIENT);
+
+  // 4:accept
+  struct sockaddr_in client_addr;
+  socklen_t len = sizeof(client_addr);
+  int client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &len);
+
+  // send
+  write(client_socket, "Hello", 5);
+
+  // close client connection
+  close(client_socket);
+
+  // close listen
+  close(server_socket);
+
+  return 0;
+}
