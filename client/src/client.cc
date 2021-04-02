dae2f40a8c41ba2d29b6385b8b2fa518a14edaa6
ncradle <shinmy.mt555@gmail.com>
2021-04-03 00:34:41 +0900

    [add]error process
    add socket error process

--- client/src/client.cc
+++ client/src/client.cc
@@ -4,22 +4,41 @@ int main()
 {
   const uint16_t SERVER_PORT = 12345;
   const char LOACLHOST[] = "127.0.0.1";
+  int err = 0;
 
   // 1:socket
   int client_socket = socket(AF_INET, SOCK_STREAM, 0);
+  if (client_socket < 0)
+  {
+    perror("socket");
+    return -1;
+  }
 
   // 2:connect
   struct sockaddr_in server_addr;
   server_addr.sin_family = AF_INET;
   server_addr.sin_addr.s_addr = inet_addr(LOACLHOST);
   server_addr.sin_port = htons(SERVER_PORT);
-  connect(client_socket, (struct sockaddr *)&server_addr, sizeof(server_addr));
+  err = connect(client_socket, (struct sockaddr *)&server_addr, sizeof(server_addr));
+  if (err < 0)
+  {
+    perror("connect");
+    return -1;
+  }
 
   // read
   char buf[10] = {};
-  read(client_socket, buf, 10);
-  std::cout << buf << std::endl;
+  int read_num = 0;
+  read_num = read(client_socket, buf, 10);
+  int(read_num < 0){
+      perror("read")} std::cout
+      << buf << std::endl;
 
-  close(client_socket);
+  err = close(client_socket);
+  if (err < 0)
+  {
+    perror("close client");
+    return -1;
+  }
   return 0;
 }
--- server/src/server.cc
+++ server/src/server.cc
@@ -4,15 +4,27 @@ int main()
 {
   const uint16_t SERVER_PORT = 12345;
   const int MAX_CLIENT = 5;
+  int err = 0;
 
   // 1:socket
   // A socket is created with socket
   // AF_INET     : ipv4
   // SOCK_STREAM : tcp
   int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+  if (server_socket < 0)
+  {
+    perror("socket");
+    return -1;
+  }
 
   int yes = 1;
   err = setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes));
+  if (err < 0)
+  {
+    perror("socket option");
+    return -1;
+  }
+
   // 2:bind
   // The socket is bound to a local address using bind
   // so that other sockets may be "connect"ed to it.
@@ -20,26 +32,58 @@ int main()
   server_addr.sin_family = AF_INET;
   server_addr.sin_port = htons(SERVER_PORT);
   server_addr.sin_addr.s_addr = INADDR_ANY;
-  bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr));
+  err = bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr));
+  if (err < 0)
+  {
+    std::cout << errno << std::endl;
+    perror("bind");
+    return -1;
+  }
 
   // 3:listen
   // A willingness to accept incoming connections and
   // a queue limit for incoming connections are specified with listen
-  listen(server_socket, MAX_CLIENT);
+  err = listen(server_socket, MAX_CLIENT);
+  if (err < 0)
+  {
+    perror("listen");
+    return -1;
+  }
 
   // 4:accept
   struct sockaddr_in client_addr;
   socklen_t len = sizeof(client_addr);
   int client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &len);
+  if (client_socket < 0)
+  {
+    perror("accept");
+    return -1;
+  }
 
   // write
-  write(client_socket, "Hello", 5);
+  int write_num = 0;
+  write_num = write(client_socket, "Hello", sizeof("Hello"));
+  if (write_num < 0)
+  {
+    perror("write");
+    return -1;
+  }
 
   // close client connection
-  close(client_socket);
+  err = close(client_socket);
+  if (err < 0)
+  {
+    perror("close client");
+    return -1;
+  }
 
   // close listen
-  close(server_socket);
+  err = close(server_socket);
+  if (err < 0)
+  {
+    perror("close server");
+    return -1;
+  }
 
   return 0;
 }
