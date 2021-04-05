d7a4337cbffc575663723b0984d32e643b8bacf9
ncradle <shinmy.mt555@gmail.com>
2021-04-06 01:39:28 +0900

    [change]change thread function class grammer
    change thread function class grammer
    for using lambda function

--- src/socket_thread.cc
+++ src/socket_thread.cc
@@ -8,8 +8,8 @@ int main()
   Client client;
   server.Init();
   client.Init();
-  std::thread th0(&Client::Main, &client);
-  std::thread th1(&Server::Main, &server);
+  std::thread th0([&server] { server.Main(); });
+  std::thread th1([&client] { client.Main(); });
 
   th0.detach();
   th1.detach();
